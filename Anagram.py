37e11e07698c7ffab6270e13408f89c1988b952b
mab1290 <mab1290@rit.edu>
2014-08-24 17:29:47 -0400

    renamed working variables, fixed capitalization bug

--- Anagram.py
+++ Anagram.py
@@ -2,27 +2,27 @@ def result(tf, first, second):
     if tf == True:
         print first.capitalize() + " and " + second.capitalize() + " are anagrams."
     else:
-        print first.capitalize() + " and " + second.capitalize() + " are not anagrams."
+        print second.capitalize() + " and " + second.capitalize() + " are not anagrams."
 
 def IsAnagram(incoming):
     first = incoming[0]
     second = incoming[1]
-    firstw = first.replace(" ", "")
-    secondw = second.replace(" ", "")
+    first_working = first.replace(" ", "")
+    second_working = second.replace(" ", "")
     
     tf = False;
-    if len(firstw) != len(secondw):
+    if len(first_working) != len(second_working):
         result(tf, first, second)
         
     else:
         dict1 = dict()
         dict2 = dict()
-        for letter in firstw:
+        for letter in first_working:
             if not dict1.has_key(letter):
                 dict1[letter] = 1
             else: dict1[letter] +=1
             
-        for letter in secondw:
+        for letter in second_working:
             if not dict2.has_key(letter):
                 dict2[letter] = 1
             else: dict2[letter] +=1
